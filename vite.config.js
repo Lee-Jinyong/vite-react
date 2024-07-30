04ae89328d1b6aeecd3d9134bfcfdf37082d8430
Lee-Jinyong <yjy1480@gmail.com>
2024-07-30 19:30:28 +0900

    JSX Runtime Automatic 명시

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const viteConfig = defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
 });
 
 export default viteConfig;
