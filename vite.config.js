eaf117d924e119bd209d7af04eb87314c2d8f5ca
Lee-Jinyong <yjy1480@gmail.com>
2024-07-30 20:26:24 +0900

    절대 경로 설정
    - vite에서 @를 src 경로로 인식하도록 설정
    - VS Code에서 @를 src 경로로 인식하도록 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 const viteConfig = defineConfig({
   plugins: [
@@ -7,6 +8,11 @@ const viteConfig = defineConfig({
       jsxRuntime: "automatic",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
 
 export default viteConfig;
